b0baeff8c032233729b07f141ebf7818e69c80d3
SOYEONG OH <axionoso@gmail.com>
2019-03-25 16:00:29 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,20 +6,30 @@ def second_challenge
         :age => "53"
       },
       :matriarch => {
-        :name => "Lady Montague"
+        :name => "Lady Montague",
         :age => "54"
       },
       :hero => {
-        :name => "Romeo"
-        :age => "15"
+        :name => "Romeo",
+        :age => "15",
         :status => "alive"
       },
       :hero_friends => {}
       },
     :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
+      :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
+      },
+      :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
+      },
+      :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
+      },
       :heroine_friends => {}
       }}
 
